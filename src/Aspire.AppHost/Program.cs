0b2f1d4581f8d85adb83fd9095c338350915cc8d
omerkocadere <okocadere@gmail.com>
2025-07-07 21:12:06 +0300

    refactor(docker-compose): update PostgreSQL volume configuration and parameters
    - Changed the PostgreSQL volume from a named volume to a bind mount for better data management.
    - Added parameters for PostgreSQL username and password to enhance security and configuration clarity.
    - No change to the existing database functionality, but improves the setup for local development.

--- src/Aspire.AppHost/Program.cs
+++ src/Aspire.AppHost/Program.cs
@@ -1,14 +1,17 @@
+// Program.cs (AppHost projesi)
 var builder = DistributedApplication.CreateBuilder(args);
 
+// PostgreSQL şifresini parameter olarak tanımlayın
+var postgresPassword = builder.AddParameter("postgres-password", "postgrespw", secret: true);
+var postgresUserName = builder.AddParameter("postgres-username", "postgres", secret: true);
+
+// PostgreSQL container'ını oluşturun ve veritabanını ekleyin
 IResourceBuilder<PostgresDatabaseResource> database = builder
-    .AddPostgres("database")
+    .AddPostgres("database", userName: postgresUserName, password: postgresPassword, port: 5432)
     .WithBindMount("../../.containers/db", "/var/lib/postgresql/data")
     .AddDatabase("OmCleanArch");
 
-builder
-    .AddProject<Projects.Web_Api>("web-api")
-    .WithEnvironment("DatabaseSettings__PostgresConnectionString", database)
-    .WithReference(database)
-    .WaitFor(database);
+// API projenize referans verin
+builder.AddProject<Projects.Web_Api>("web-api").WithReference(database).WaitFor(database);
 
 builder.Build().Run();
