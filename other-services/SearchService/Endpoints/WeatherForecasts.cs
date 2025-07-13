09c6d3cdeb19d8353262a34c7519076052500d42
omerkocadere <okocadere@gmail.com>
2025-07-14 00:14:13 +0300

    feat(searchService): update search and weather forecast endpoints
    - Changed the route for search endpoint to "api/search" for better clarity and organization.
    - Updated the weather forecast endpoint to "/weatherforecast" for consistency in API design.
    - Modified SearchParams to allow nullable PageNumber and PageSize for more flexible query handling.
    - Ensured default values are maintained when parameters are not provided.

--- other-services/SearchService/Endpoints/WeatherForecasts.cs
+++ other-services/SearchService/Endpoints/WeatherForecasts.cs
@@ -8,7 +8,7 @@ public static class WeatherForecasts
 
     public static void MapWeatherForecastEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("", GetWeatherForecast).WithTags("WeatherForecast");
+        endpoints.MapGet("/weatherforecast", GetWeatherForecast).WithTags("WeatherForecast");
     }
 
     private static WeatherForecast[] GetWeatherForecast()
