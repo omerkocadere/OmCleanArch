ef3ee89b1c5850be4ff273985acf2872e294aab0
omerkocadere <okocadere@gmail.com>
2025-07-13 16:06:47 +0300

    feat(weatherForecast): add weather forecast endpoint and model
    - Implemented a new endpoint for retrieving weather forecasts.
    - Created a `WeatherForecast` model to structure the forecast data.
    - Added MongoDB.Entities package for potential future database integration.

---
+++ other-services/SearchService/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,27 @@
+using SearchService.Models;
+
+namespace SearchService.Endpoints;
+
+public static class WeatherForecastEndpoints
+{
+    private static readonly string[] Summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Hot"];
+
+    public static void MapWeatherForecastEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/weatherforecast", GetWeatherForecast).WithTags("WeatherForecast");
+    }
+
+    private static WeatherForecast[] GetWeatherForecast()
+    {
+        var forecast = Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Length)]
+            ))
+            .ToArray();
+
+        return forecast;
+    }
+}
