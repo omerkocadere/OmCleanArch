e16132a6f89281d8a29625777502f72ecb9602fa
omerkocadere <okocadere@gmail.com>
2025-08-02 11:32:31 +0300

    refactor(auctions): streamline auction and item configuration
    - Removed commented-out code for clarity and maintainability
    - Consolidated navigation property configuration in AuctionConfiguration

--- src/Infrastructure/Data/Configurations/AuctionConfiguration.cs
+++ src/Infrastructure/Data/Configurations/AuctionConfiguration.cs
@@ -1,4 +1,5 @@
 using CleanArch.Domain.Auctions;
+using CleanArch.Domain.Items;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,20 +9,10 @@ public class AuctionConfiguration : IEntityTypeConfiguration<Auction>
 {
     public void Configure(EntityTypeBuilder<Auction> builder)
     {
-        // builder.HasKey(x => x.Id);
-        // builder.Property(x => x.Seller).IsRequired().HasMaxLength(100);
-        // builder.Property(x => x.Winner).HasMaxLength(100);
-        // builder.Property(x => x.ReservePrice).IsRequired();
-        // builder.Property(x => x.Status).HasConversion<string>().IsRequired();
-        // builder.Property(x => x.CreatedAt).IsRequired();
-        // builder.Property(x => x.UpdatedAt).IsRequired();
-        // builder.Property(x => x.AuctionEnd).IsRequired();
-
-        // Navigation properties
-        // builder
-        //     .HasOne(x => x.Item)
-        //     .WithOne(x => x.Auction)
-        //     .HasForeignKey<CleanArch.Domain.Items.Item>(x => x.AuctionId)
-        //     .OnDelete(DeleteBehavior.Cascade);
+        builder
+            .HasOne(x => x.Item)
+            .WithOne(x => x.Auction)
+            .HasForeignKey<Item>(x => x.AuctionId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- src/Infrastructure/Data/Configurations/ItemConfiguration.cs
+++ src/Infrastructure/Data/Configurations/ItemConfiguration.cs
@@ -6,16 +6,5 @@ namespace CleanArch.Infrastructure.Data.Configurations;
 
 public class ItemConfiguration : IEntityTypeConfiguration<Item>
 {
-    public void Configure(EntityTypeBuilder<Item> builder)
-    {
-        // builder.HasKey(x => x.Id);
-        // builder.Property(x => x.Make).IsRequired().HasMaxLength(50);
-        // builder.Property(x => x.Model).IsRequired().HasMaxLength(50);
-        // builder.Property(x => x.Year).IsRequired();
-        // builder.Property(x => x.Color).IsRequired().HasMaxLength(30);
-        // builder.Property(x => x.Mileage).IsRequired();
-        // builder.Property(x => x.ImageUrl).IsRequired().HasMaxLength(500);
-
-        // Navigation properties are handled in AuctionConfiguration
-    }
+    public void Configure(EntityTypeBuilder<Item> builder) { }
 }
