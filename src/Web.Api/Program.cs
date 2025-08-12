f6ef7395f2e26a0ec3da5601cdd67bc67940ac94
omerkocadere <okocadere@gmail.com>
2025-08-12 19:13:19 +0300

    feat(ci): add GitHub Actions workflow for publishing
    - Implemented a CI workflow to automate the publishing process.
    - Supports manual triggers and pushes to the main-enhanced branch.
    - Ensures consistent builds and deployments using .NET SDK 9.0.

--- src/Web.Api/Program.cs
+++ src/Web.Api/Program.cs
@@ -53,17 +53,8 @@ app.UseExceptionHandler();
 
 await app.InitialiseDatabaseAsync();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
-{
-    app.UseHangfireDashboardConditionally();
-    app.UseSwaggerWithUi();
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseHangfireDashboardConditionally();
+app.UseSwaggerWithUi();
 
 app.UseHttpsRedirection();
 
