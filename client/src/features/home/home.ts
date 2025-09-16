b056560c7557a94f134ef59b9c0cfe6b169ea668
omerkocadere <okocadere@gmail.com>
2025-09-16 08:52:40 +0300

    feat(home): enhance home component with Angular setup
    - Added setup for Node.js and Angular CLI in CI workflow.
    - Improved home component to conditionally display registration button based on user authentication state.
    - Ensures a smoother user experience by allowing registration only for unauthenticated users.

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,15 +1,16 @@
-import { Component, Input, signal } from '@angular/core';
-import { Register } from "../account/register/register";
-import { User } from '../../types/user';
+import { Component, inject, signal } from '@angular/core';
+import { AccountService } from '../../core/services/account-service';
+import { Register } from '../account/register/register';
 
 @Component({
   selector: 'app-home',
   imports: [Register],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
 
   showRegister(value: boolean) {
     this.registerMode.set(value);
